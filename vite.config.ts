becef7ea43c94149de517c0c77b263f26078e329
yifeicosmos <yifeizhang1314@gmail.com>
2026-09-06 20:50:48 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-   base: '/pythagoras-numerology/',
   plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
